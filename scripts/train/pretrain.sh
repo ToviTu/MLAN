e63f09f87eb02947a5bf40c6c93e75f9c8541f3b
tovitu <tovitusaikou@gmail.com>
2024-11-11 13:03:30 -0600

    updated scripts

--- scripts/train/pretrain.sh
+++ scripts/train/pretrain.sh
@@ -11,15 +11,21 @@ WORKING_DIR=$PWD
 STORAGE_DIR=$PWD
 
 # Ensure per_device_train_batch_size*num_devices*gradient_accumulation_steps is 256
+
+BASE_MODEL=meta-llama/Llama-2-7b-hf
+PRETRAINED_MODEL=llava-llama2-7b-pretrain
+
+# BASE_MODEL=lmsys/vicuna-7b-v1.5
+# PRETRAINED_MODEL=llava-vicuna-7b-pretrain
  
 deepspeed --master_port=7000 \
     --include=localhost:0,1,2,3,4,5,6,7 \
     ${WORKING_DIR}/llava/train/train_mem.py \
     --deepspeed ${WORKING_DIR}/scripts/config/zero2.json \
-    --model_name_or_path meta-llama/Llama-2-7b-hf \
+    --model_name_or_path $BASE_MODEL \
     --version plain \
-    --data_path ${STORAGE_DIR}/datasets/llava/blip_laion_cc_sbu_558k.json \
-    --image_folder ${STORAGE_DIR}/datasets/llava/images \
+    --data_path ${STORAGE_DIR}/playground/data/blip_laion_cc_sbu_558k.json \
+    --image_folder ${STORAGE_DIR}/playground/data/images \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --freeze_backbone True \
@@ -29,7 +35,7 @@ deepspeed --master_port=7000 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir ${STORAGE_DIR}/models/llava-llama2-7b-pretrain \
+    --output_dir ${STORAGE_DIR}/playground/models/${PRETRAINED_MODEL} \
     --num_train_epochs 1 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 4 \
